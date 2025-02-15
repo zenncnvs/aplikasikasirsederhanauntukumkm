f05b7385be42451310b1b5973a26a17d2d3ecc6c
Aisah Gandari Rahmah <102279299+zenncnvs@users.noreply.github.com>
2025-02-15 17:27:26 +0700

    Add files via upload

---
+++ kasir_umkm.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 15, 2025 at 11:26 AM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kasir_umkm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id_barang`, `nama_barang`, `harga`, `status`) VALUES
+(1, 'JasJus Jeruk', 11000, 'TERSEDIA'),
+(6, 'Mie Sedap Goreng', 4000, 'TERSEDIA'),
+(7, 'Mie Sedap Soto', 17000, 'TERSEDIA'),
+(8, 'Gentle Gen', 12000, 'TERSEDIA'),
+(9, 'Downy', 25000, 'TERSEDIA'),
+(10, 'Pepsodent', 7000, 'TERSEDIA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `nama_pelanggan` varchar(50) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_barang` varchar(30) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah_beli` int(11) NOT NULL,
+  `total_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `nama_pelanggan`, `id_barang`, `tanggal`, `nama_barang`, `harga`, `jumlah_beli`, `total_bayar`) VALUES
+(9, 'Jackson', 8, '2025-01-13', 'Gentle Gen', 12000, 2, 24000),
+(10, 'ian', 6, '2025-01-15', 'Mie Sedap Goreng', 4000, 2, 8000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `nama_user` varchar(30) NOT NULL,
+  `id_level` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `username`, `password`, `nama_user`, `id_level`) VALUES
+(1, 'admin', 'admin', 'Ian Sunandar', 1),
+(3, 'kasir', 'kasir', 'M Raihan Samih', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD UNIQUE KEY `UNIQE` (`nama_pelanggan`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
